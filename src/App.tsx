1f2c2ba5c0368f0033bcf17d49a51ef376d983d9
sod7m <dmytrofeedback@gmail.com>
2025-05-31 20:09:39 +0300

    feat: add How It Works page and integrate into routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -10,6 +9,7 @@ import RegisterInfluencer from "./pages/RegisterInfluencer";
 import RegisterBrand from "./pages/RegisterBrand";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
+import HowItWorks from "./pages/HowItWorks";
 
 const queryClient = new QueryClient();
 
@@ -24,6 +24,7 @@ const App = () => (
           <Route path="/catalog" element={<Catalog />} />
           <Route path="/register-influencer" element={<RegisterInfluencer />} />
           <Route path="/register-brand" element={<RegisterBrand />} />
+          <Route path="/how-it-works" element={<HowItWorks />} />
           <Route path="/login" element={<Login />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
