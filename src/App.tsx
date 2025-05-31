aa8205915b914e8a2cd3fc05919cbe7543fd1621
sod7m <dmytrofeedback@gmail.com>
2025-05-31 20:13:10 +0300

    feat: add ScrollToTop component and integrate into App routing

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import RegisterBrand from "./pages/RegisterBrand";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 import HowItWorks from "./pages/HowItWorks";
+import ScrollToTop from "@/components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
@@ -19,6 +20,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/catalog" element={<Catalog />} />
